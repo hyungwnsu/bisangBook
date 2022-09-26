ad84dc50dc64be55646b895762300bc16ef6edd2
hyungwnsu <joon7joon@naver.com>
2022-09-26 23:27:20 +0900

    게시글 업로드, 수정 및 삭제

--- src/main/java/com/example/bisang/BisangApplication.java
+++ src/main/java/com/example/bisang/BisangApplication.java
@@ -1,13 +1,15 @@
 package com.example.bisang;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BisangApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BisangApplication.class, args);
 	}
-
 }
